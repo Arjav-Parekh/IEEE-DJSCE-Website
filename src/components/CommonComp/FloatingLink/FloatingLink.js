7aaf9f1a7a5bf10c643ea2753ff31823e8ae9a92
Manan Doshi <manandoshi1301@gmail.com>
2021-12-14 16:38:08 +0530

    Updated some links

--- src/components/CommonComp/FloatingLink/FloatingLink.js
+++ src/components/CommonComp/FloatingLink/FloatingLink.js
@@ -3,13 +3,23 @@ import styles from './FloatingLink.module.scss'
 <link rel="stylesheet" href="https://kit.fontawesome.com/a28f231496.js" />
 
 function FloatingLink() {
+    const socialLinks = [
+        { name: "Instagram", linkTo: "#", iconClass: "fa-brands fa-instagram", color: "" },
+        { name: "LinkedIn", linkTo: "#", iconClass: "fab fa-linked-in", color: "" },
+        { name: "Twitter", linkTo: "#", iconClass: "fab fa-twitter", color: "" },
+        { name: "Medium", linkTo: "#", iconClass: "fa-brands fa-medium", color: "" },
+        { name: "Gmail", linkTo: "#", iconClass: "fab fa-envelope", color: "" },
+    ]
+
     return (
         <div className={styles.icons}>
-            <a href="#"><i class="fab fa-medium-m"></i></a> 
-            <a href="#"><i class="fab fa-instagram"></i></a>
-            <a href="#"><i class="fab fa-linkedin-in"></i></a>
-            <a href="#"><i class="fab fa-twitter"></i></a>
-            <a href="#"><i class="fas fa-envelope"></i></a>
+            {
+                socialLinks.map((item, key) => (
+                    <a href={item.linkTo} key={key}>
+                        <i className={item.iconClass}></i>
+                    </a>
+                ))
+            }
         </div>
     )
 }
