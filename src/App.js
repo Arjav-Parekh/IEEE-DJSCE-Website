52e2017c178eda26a60d172c657e4abac85998b2
Manan Doshi <manandoshi1301@gmail.com>
2022-01-14 21:39:00 +0530

    Add permanent body back black color

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import styles from "./App.module.scss";
 import ContextComponentHolder from './components/ContextComponentHolder';
 
 function App() {
+
+  document.body.style.backgroundColor = "black";
+  
   return (
     <div className={cx(styles.App)}>
       {/* <p className={cx(styles.para)}>Hello this is just for example</p> */}
