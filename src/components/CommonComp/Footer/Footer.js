ecc0ae3f792dcc2f203ba5c7ca60eab7b9cad6ed
Manan Doshi <manandoshi1301@gmail.com>
2022-02-04 20:00:30 +0530

    Footer configured

--- src/components/CommonComp/Footer/Footer.js
+++ src/components/CommonComp/Footer/Footer.js
@@ -1,19 +1,71 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from "./Footer.module.scss";
-import img from "../../../images/Footer_withoutBg_cropped.png";
+import { Context } from '../../ContextData'
+import { Link } from 'react-router-dom';
+// import img from "../../../images/Footer_withoutBg_cropped.png";
 
 const Footer = () => {
 
-  const footerStyles = {
-    backgroundSize: 'cover',
-    backgroundImage: `url(${img})`,
-    backgroundPosition: 'center center',
-  }
+  const [baseData] = useContext(Context);
+  const IEEELogoImg = baseData.navLogo;
+
+  const footerAboutLinks = [
+    { name: "About Us", link: "aboutUs" },
+    { name: "Events", link: "events" },
+    { name: "Sponsors", link: "sponsors" },
+    { name: "Our Team", link: "teams" },
+  ]
 
   return (
-    <footer className={styles.footer} style={footerStyles}>
+    <footer className={styles.footer}>
+      <div className={styles.footerDiv}>
 
-    </footer>
+        <div className={styles.footerBrand}>
+          <img src={IEEELogoImg} alt="" />
+        </div>
+
+        <div className={styles.footerElements}>
+
+          <div>
+            <h1>About</h1>
+            <div className={styles.footerLinks}>
+              {
+                footerAboutLinks.map((item, key) => (
+                  <div className={styles.footerRouteLink} key={key}>
+                    <Link to={`/${item.link}`}>{item.name}</Link>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+
+          <div>
+            <h1>Contact</h1>
+            <div className={styles.footerLinks}>
+              <div>Email</div>
+              <div>Ambar Mehta: </div>
+              <div>Naman Sanghvi: </div>
+            </div>
+          </div>
+
+          <div>
+            <h1>Address</h1>
+            <div className={styles.footerLinks}>
+              <div>Dwarkadas J. Sanghvi College of Engineering, Vile Parle West, Mumbai-400056</div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className={styles.copyContent}>
+          <div><hr /></div>
+          <div className={styles.footerCopyright}>
+            &copy; 2020 DJSCE IEEEBainwaves - All Rights Reserved
+            {/* <br /> */}
+          </div>
+        </div>
+      </div>
+    </footer >
   )
 }
 
--- src/components/Home/UpcomingEvent/UpcomingEvent.js
+++ src/components/Home/UpcomingEvent/UpcomingEvent.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import img from './UpcomingEvent.png'
+// import img from './UpcomingEvent.png'
 import styles from './UpcomingEvent.module.scss'
 
-export default function UpcomingEvent({data}) {
+export default function UpcomingEvent({ data }) {
     return (
         <div className={styles.upcomingEvent}>
             <h1 className={styles.title}>Upcoming Event</h1>
             <div className={styles.contentDiv}>
                 <div className={styles.imgWrap}>
-                    <img src={data.posterImg} alt="Upcoming Event" className={styles.image}/>
+                    <img src={data.posterImg} alt="Upcoming Event" className={styles.image} />
                 </div>
                 <div className={styles.btnWrap}>
                     <button>Register</button>
